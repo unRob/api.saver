9bb464bf35058c8c77882a6a02d63a9175de5026
rob <rob@partidosurrealista.com>
2016-03-04 17:39:36 -0600

    Initial Commit

---
+++ APIScreenSaver/APIScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  APIScreenSaverView.h
+//  APIScreenSaver
+//
+//  Created by Roberto Hidalgo on 3/4/16.
+//  Copyright © 2016 Roberto Hidalgo. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface APIScreenSaverView : ScreenSaverView
+
+@end
---
+++ APIScreenSaver/APIScreenSaverView.m
@@ -0,0 +1,52 @@
+//
+//  APIScreenSaverView.m
+//  APIScreenSaver
+//
+//  Created by Roberto Hidalgo on 3/4/16.
+//  Copyright © 2016 Roberto Hidalgo. All rights reserved.
+//
+
+#import "APIScreenSaverView.h"
+
+@implementation APIScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
